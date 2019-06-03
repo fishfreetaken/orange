394f5f88c3cbe87dab58321705517047cb59bf63
chendong <471088280@qq.com>
2019-06-03 19:23:47 +0800

    修改一下统一的包结构体

--- Emperor/util.h
+++ Emperor/util.h
@@ -3,23 +3,24 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 
+#include <limits.h>
 #include <errno.h>
 #include <string.h>
 #include <string>
+#include <stddef.h>
 
 #include <stdio.h>
 #include <unistd.h>
 #include <netdb.h>
 #include <fcntl.h>
 #include <iostream>
-#include "log.h"
 
+#include "genrandom.h"
 #include <vector>
 #include <thread>
 
 #include <sys/epoll.h>
 
-#define UTILLOGLEVEL1 1
 
 #define UTILNET_SUCCESS  0
 #define UTILNET_ERROR  -1
@@ -27,25 +28,21 @@
 #define SERVERLISTENIP    "localhost"
 #define SERVERLISTENPORT 8888
 
+#define LOADCHARLEN 150
+
 int tcpGenericServer(const char *source_addr,int port);
 int tcpGenericConnect(const char *source_addr,int port,const char *dest_ip,int dest_port);
 void setNonBlock(int socket_fd);
 int writeGenericSend(int fd,const char * buf,int len);
 
-typedef struct transferObj {
-    uint id; //=1
-    int from;
-    int to;
-    std::string buf_;
-} transfObjInfo;
 
 //统一的一个包头结构
 typedef struct transferOnlinePersion{
     uint id; //=2
-    int from;
-    int to;
-    int size;
-    char list_[100];
+    size_t uid;
+    size_t to;
+    size_t size;
+    char info_[LOADCHARLEN];
 } transfOnPer;
 
 #define STRUCTONPERLEN  sizeof(transfOnPer)
