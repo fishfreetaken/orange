3cacc6426f93c6c6e326f0edad09adb7d9f72239
chendong <471088280@qq.com>
2019-06-12 17:41:23 +0800

    channel

--- Emperor/channel.h
+++ Emperor/channel.h
@@ -1,5 +1,9 @@
 
-
+#define USERSUCCESS         0
+#define USERFDREADFAIL      1
+#define USERBUFDECRYPTFAIL  2
+#define USERNEWFAIL         3
+ 
 
 class channel;
 class user{
@@ -25,6 +29,8 @@ public:
     void SetOnLine();
     void setOffLine();
 
+    bool WetherOnline();
+
 private:
     int SendBuf(const char* buf,size_t len);
 
@@ -33,13 +39,16 @@ private:
 private :
     size_t uid_; //使用者的uid
     std::unoredered_map<size_t,user*> partnermap; //将所有的使用一个map进行管理起来,没上线的nullptr
-    
+
+    char * buffer_;
+
+
     //channel *chmt; //用来进行当前所有在线用户的管理；单例模式
     int fd_;
 };
 
 /*
-管理所有的成员
+管理所有的成员，进行线程池管理，如果线程池满了，加入一个等待处理队列中去；
 */
 class channel{
 public:
@@ -47,35 +56,31 @@ public:
 
     static channel* GetInstance();
 
-    /*
-        该频道所有用户发送消息；
-        返回值：发送的用户数；
-        入参：无；
-    */
-    size_t Broadcast();
-
-    int SendToOne(size_t uid, const char * buf,size_t len);
+    /*传递给user去处理fd事件协议 */
+    int UserReadProtocl(int fd);
 
     /*
         从文件，或者mysql加载已经注册的有效用户
     */
-    int LoadPartnerMap();
-
-    user *operator[](size_t &t);
+    //int LoadPartnerMap(); //没有必要所有的都进行加载，进行动态的增加用户和释放
 
     /*
         如果有一个partner登录进入服务器，进行验证加入群组；
         返回值：
     */
-
     user* UserOnlineReg(size_t &uid,int fd);
     void UserOfflineReg(size_t &uid);
 
 private:
-    int IncOnePartner(user*p);
-    int DelOnePartner(int fd);
+    int IncOneUser(user*p);
+    int DelOneUser(int fd);
 
-private:
-    std::unoredered_map<size_t,user*> usermap;//该chennl下所有的partner,由文件描述符进行索引
+private: //能支持1千万的并发量就已经很不错了！
+    std::unordered_map<int,user*> fdmapuser_;  //该chennl下所有的partner,由客户端文件描述符进行索引 fd->id->fd进行转发消息；
+
+    std::unordered_map<size_t,user*> idmapuser_; //管理当前所有的user，由user id 进行索引；
+
+/*通过参数配置的方式进行线程池的配置初始化 */
+    std::thread *threadpool_;
 
 };
