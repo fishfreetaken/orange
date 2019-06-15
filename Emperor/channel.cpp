b6ab58c3561048ccf55fb1340baeb1b76bfd8617
chendong <471088280@qq.com>
2019-06-15 17:46:34 +0800

    完善

--- Emperor/channel.cpp
+++ Emperor/channel.cpp
@@ -7,12 +7,15 @@
 user::user(int fd):
 fd_(fd),
 uid_(0),
-buffer_()  //使用主备定义好的消息协议
+buffer_(nullptr),  //使用主备定义好的消息协议
+key_(nullptr)
 {
+
 }
 
 user::~user()
 {
+    InformPartnerOnOrOff(0);
     if(buffer_!=nullptr)
     {
         delete [] buffer_;
@@ -54,13 +57,12 @@ int user::ReadBufferParse()
         return USERFDREADFAIL;
     }
 
-    /*解密并进行校验如果出错直接返回失败 */
-    ret=BufferDecryptCrc();
-    if(ret <0)
-    {
-        return ret;
-    }
-
+    /*解密并进行校验如果出错直接返回失败，将buf解密后重新写入更新 */
+    //ret=BufferDecryptCrc(); 
+    //if(ret <0)
+    //{
+    //    return ret;
+    //}
     ret=ParsePacket();
     if(ret <0)
     {
@@ -70,65 +72,71 @@ int user::ReadBufferParse()
     return ret;
 }
 
-int user::ParsePacket(transfOnPer *info)
+int user::ParsePacket()
 {
     transfOnPer *info=(transfOnPer *) buffer_;
+    int ret=0;
 
     if(uid_==0)
     {
         /*从文件或者数据库中进行加载uid信息 */
-        LoadUserInfoFromDb(info->uid);
+        ret= LoadUserInfoFromDb(info->uid);
+        if( USERSUCCESS !=ret )
+        {
+            LOG::record(UTILLOGLEVEL1,"user::ParsePacket NotFoundUid uid:%zu",info->udi);
+            return ret;
+        }
     }
 
     if(info->uid!=uid_)
     {
-        LOG::record(UTILLOGLEVEL1,"user::sendto info uid is mismatch: s:%zu d:%zu",info->udi,uid_);
-        return -1;
+        LOG::record(UTILLOGLEVEL1,"user::ParsePacket uid is mismatch: remote:%zu local:%zu",info->udi,uid_);
+        return USERIDMISMATCH;
     }
 
     switch (info->fd)
     {
         case 0: //心跳包；应该需要应答
             break;
-        case 1: //转发消息包；
-            SendTo(info);
+        case 1: //好友目标 转发消息包；
+            SendTo();
             break;
-        case 2: //广播包
+        case 2: //群消息包；
             break;
-
+        case 3: //公钥加密，协商对称秘钥
+            break;
         default:
-            LOG::record(UTILLOGLEVEL1,"not recognise id");
+            LOG::record(UTILLOGLEVEL1,"ParsePacket not recognised msg id");
+            return USERFAILED;
             break;
     }
     return 0;
 }
 
-int user::SendTo(transfOnPer *info)
+int user::LoadUserInfoFromDb()
 {
-    //int JudegeIfMyFriend();
-    //int ret=GetInstance()->SendToOne(info);
-    /*
-    if(GetInstance()->find(info->to)==-1)
-    {
-        //对方不在线，将消息投递到一个消息队列中。
-        return 0;
-    }
-    //将消息通过接口发送给对方；
-    int ret=partermap[info->to]->SendMyself(info);*/
+    
+}
+
+int user::SendTo()
+{
+    transfOnPer *info= (transfOnPer *)buffer_;
 
     if(partnermap.find(info->to)==partnermap.end())
     {
         //找不到；
-        return -1;
+        return USERNOTMYFIREND;
     }
 
     if(partnermap[info->to]==nullptr)
     {
-        //投递到消息队列，等待上线进行处理，同事写数据库；
+        //投递到消息队列，等待上线进行处理，同时写数据库；
         return 0;
     }
 
     partnermap[info->to].SendMyself(info);
+
+    //DbFileStore(); //数据库操作进行数据存储
 
     return ret;
 }
@@ -138,25 +146,22 @@ int user::SendMyself(transfOnPer *info)
     if(info==NULL)
     {
         LOG::record(UTILLOGLEVEL1,"%s para is NULL",__FUNCTION__);
-        return -1;
+        return USERPOINTNULL;
     }
     if(info->to!=uid_)
     {
         LOG::record(UTILLOGLEVEL1,"%s uid mismatch %zu",__FUNCTION__,info->to);
-        return;
+        return USERIDMISMATCH;
     }
-    if(fd_<=2)
-    {//文件描述符失效，发送失败，将消息发送到数据库中，或者发送到消息队列中；
+    //BufferCryptCrc();//测试的时候进行不处理加密功能，保留接口
 
-        return;
-    }
     SendBuf(info->buf,info->size);
 }
 
-int user::SendBuf(const char* buf,size_t len)
+int user::SendBuf(size_t len)
 {
     int ret=0;
-    ret = ::write(fd_,buf,len);
+    ret = ::write(fd_,buffer_,len);
     if(ret<0)
     {
         LOG::record(UTILLOGLEVEL1,"user::SendBuf :%d errnor:%s",errno,strerror(errno));
@@ -180,84 +185,93 @@ size_t user::SendToAll(transfOnPer *info)
     return cc;
 }
 
-void user::InfoFriendOnline(size_t uid,int fd)
+int user::InformPartnerOnOrOff(int ret)
 {
-    if(uid==uid_)
+    int cc=0;
+    for(auto it=partnermap.begin();it!=partnermap.end();it++)
     {
-        
+        if(it->second!=nullptr)
+        {
+            if(ret)
+            {
+                it->second->InformPartnerOnline(this,uid_); //通知同伴自身已经上线
+            }else{
+                it->second->InformPartnerOnline(nullptr,uid_); 
+            }
+            cc++;
+        }
     }
-    return ;
+    return cc;
 }
 
-void user::SetOnLine(int&fd)
-{
-    if(fd_>=0)
-    {
-        ::close(fd_);
-        /*
-            这个不但要关闭连接，还需要关闭epoll的注册事件
-        */
-        LOG::record(UTILLOGLEVEL1,"%s close fd_: %d",__FUNCTION__,fd_);
+int user::InformPartnerOnline(user*p,size_t ret)
+{   
+    auto it=partnermap.find(ret);
+    if(it==partnermap.end()){
+        LOG::record(UTILLOGLEVEL1,"%s  not find partner :%zu",__FUNCTION__,p->ReturnUid());
+        return USERNOTFOUNDID;
     }
-    fd_=fd;
-}
-void user::SetOffLine(int& fd)
-{
-    if(fd_<=2)
-    {
-        LOG::record(UTILLOGLEVEL1,"%s close fd_: %d",__FUNCTION__,fd_);
-        return;
-    }
-    ::close(fd_);
-    fd_=-1;
+    it->second=p;
+    return  USERSUCCESS;
 }
 
-int channel::IncOnePartner(size_t uid,int fd)
+int user::GroupMyPartner()
 {
-    for(auto it=partermap.begin();it!=partermap.end();it++)
+    int ret=0;
+    for(auto it=partnermap.begin();it!=partnermap.end();it++)
     {
-        partermap[fd].InfoFriendOnline(uid,fd);
+        it->second=channel::GetInstance()->FindUid(it->first);
+        if(it->second!=nullptr)
+        {
+            ret++;
+        }
     }
-
-    if(partermap.find(fd)!=partermap.end())
-    {
-        delete partermap[fd];
-    }
-    partermap[fd]= new user(uid,fd);
-
-    return 0;
-}
-
-user* channel::UserOnlineReg(size_t &uid,int fd)
-{
-    if(partermap.find(uid)==partermap.end())
-    {
-        /*
-            如果找不到的话应该需要从mysql或者配置文件去加载这个用户
-        */
-        //LOG::record(UTILLOGLEVEL1,"%s no found uid: %zu",__FUNCTION__,uid);
-        return IncOnePartner(uid,fd);;
-    }
-
-    user*p= partermap[uid];
-    p.SetOnline(int fd);
-
-    return p;
+    InformPartnerOnOrOff(1);
+    return ret;
 }
 
 /*对协议进行一个解析，读入的数据进行 校验，解密{解密失败时候，判断为非法连接断开}，分析协议报头，处理动作： 登录-》消息转发-》跳转*/
 int channel::UserReadProtocl(int tfd)
 {
+    /*通知对应user进行处理，并接收结果 */
     if(fdmapuser_.find(tfd)==fdmapuser_.end())
     {
         /*新加入连接的一个客户端，执行协议解析 */
         fdmapuser_[tfd]=new user(tfd);
-        fdmapuser_[tfd].ParsePacket();
+        if(fdmapuser_[tfd]==nullptr)
+        {
+            return USERNEWFAIL;
+        }
+        fdmapuser_[tfd]->GroupMyPartner();
+        //fdmapuser_[tfd].InformPartner(); //通知同伴上线
     }
-    fdmapuser_[tfd].ParsePacket(int tfd);
+    int ret = fdmapuser_[tfd].ParsePacket(int tfd); //解析协议包并应答
+    if(ret<0)
+    {
+        UserRemove(tfd);
+        /*协议解析失败，无效链接，关闭端口 */
+    }
+    return ret;
 }
 
-int channel::ThreadWorkDispatch(int tfd)
+void channel::UserRemove(int tfd)
 {
+    auto it= fdmapuser_.find(tfd)
+    if(it==fdmapuser_.end())
+    {
+        return;
+    }
+    
+    delete it->second;
+    fdmapuser_.earse(it);
+}
 
+user* channel::FindUid(size_t&uid)
+{
+    auto it= idmapuser_.find(uid);
+    if(it==idmapuser_.end())
+    {
+        return nullptr;
+    }
+    return it->second;
 }
