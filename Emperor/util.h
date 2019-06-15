5879b8dd070ddea6960d1ba3fa465119075e02a8
chendong <471088280@qq.com>
2019-06-15 17:41:55 +0800

    将协议头分开单独定义

--- Emperor/util.h
+++ Emperor/util.h
@@ -22,28 +22,21 @@
 #include <sys/epoll.h>
 
 
-#define UTILNET_SUCCESS  0
-#define UTILNET_ERROR  -1
+#define UTILNET_SUCCESS      0
+#define UTILNET_ERROR       -1
+#define UTILPOINTER_NULL    -2
+#define UIIL_NOTFOUND       -3
 
-#define SERVERLISTENIP    "localhost"
+
+#define SERVERLISTENIP    "192.168.169.1"
 #define SERVERLISTENPORT 8888
 
-#define LOADCHARLEN 150
+
 
 int tcpGenericServer(const char *source_addr,int port);
 int tcpGenericConnect(const char *source_addr,int port,const char *dest_ip,int dest_port);
 void setNonBlock(int socket_fd);
 int writeGenericSend(int fd,const char * buf,int len);
+int readGenericReceive(int &fd,const char *buf,int &len);
 
-
-//统一的一个包头结构
-typedef struct transferOnlinePersion{
-    uint id; //=2
-    size_t uid;
-    size_t to;
-    size_t size;
-    char info_[LOADCHARLEN];
-} transfOnPer;
-
-#define STRUCTONPERLEN  sizeof(transfOnPer)
 
