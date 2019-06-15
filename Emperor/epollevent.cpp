149aff7ae54b3a724c9de248b21e87630bc14e75
chendong <471088280@qq.com>
2019-06-15 17:41:19 +0800

    给timeevent添加回调函数功能

--- Emperor/epollevent.cpp
+++ Emperor/epollevent.cpp
@@ -1,6 +1,7 @@
 
 #include "util.h"
 #include "log.h"
+#include "channel.h"
 
 int epollhandlebase::WaitTimeOut()
 {
@@ -68,7 +69,7 @@ void epollevent::EpollInit()
     {
         struct epoll_event ee = {0,0};
         ee.events |=  EPOLLIN | EPOLLET |EPOLLRDHUP;
-        ee.data.fd=tfd;
+        ee.data.fd=listen_fd_;
         EpollEventAdd(ee);
     }
 }
@@ -146,126 +147,8 @@ void epollevent::EpollDelEvent(int fd)
     //channel::GetInstance()->UserRemove(fd); //从队列中删除；
 }
 
-/*要快速的处理 */
-epollserverhandle::epollserverhandle(int timeout=1500): //定时延长一个1.5s
-listen_fd_(-1),
-timeout_(timeout)
-{
-}
 
-epollserverhandle::~epollserverhandle()
-{
-    if(evp_!=nullptr)
-    {
-        delete evp_;
-    }
-    if(chm_!=nullptr)
-    {
-        delete chm_;
-    }
-    if(tme_!=nullptr)
-    {
-        delete tme_;
-    }
-}
 
-int epollserverhandle::ServerStart(const char* listenip,const int port,int timeout=15000)
-{
-    listen_fd_=tcpGenericServer(SERVERLISTENIP,SERVERLISTENPORT);
-    if(fd < 0)
-    {
-        LOG::record(UTILLOGLEVEL1, "ServerStart failed %s : %s",__FUNCTION__, strerror(errno));
-        return -1;
-    }
-    evp_=new epollevent(this,listen_fd_);
-    if(evp_==nullptr)
-    {
-        LOG::record(UTILLOGLEVEL1, "ServerStart epollevent new failed %s : %s",__FUNCTION__, strerror(errno));
-        return -1;
-    }
-
-    chm_= new channel();
-    if(chm_==nullptr)
-    {
-        LOG::record(UTILLOGLEVEL1, "ServerStart channel new failed %s : %s",__FUNCTION__, strerror(errno));
-        return -1;
-    }
-
-    tme_ = new timeevent();
-    if(tme_ == nullptr)
-    {
-        LOG::record(UTILLOGLEVEL1, "ServerStart timeevent new failed %s : %s",__FUNCTION__, strerror(errno));
-        return -1;
-    }
-
-    while(1)
-    {
-        evp_->EpollEventWaite();//程序主入口
-        TimeEventsProc();
-    }
-
-    return 0;
-}
-void epollserverhandle::TimeEventsProc()
-{
-    std::vector<int> t = tme_->TimeEventProc();
-    for(auto i:t)
-    {
-        DelEvent(i);
-    }
-}
-
-void epollserverhandle::AcceptEvent()
-{
-    int tfd=0;
-    static int count=0;
-    struct sockaddr m;
-    socklen_t flag=1;
-    do{
-        tfd=accept(listen_fd_,&m,&flag);
-        if(tfd==-1)
-        {
-            if ((errno == EINTR)||(errno == EAGAIN))
-            {
-                continue;
-            }else{
-                LOG::record(UTILLOGLEVEL1,"%s accept:%s \n",__FUNCTION__,strerror(errno));
-                return; 
-            }
-        }
-        break;
-    }while(1);
-
-    setNonBlock(tfd);
-    struct epoll_event ee = {0,0};
-    ee.events |=  EPOLLIN | EPOLLET |EPOLLRDHUP;
-    ee.data.fd=tfd;
-
-    if(evp_->EpollEventAdd(ee)>0)
-    {
-        /*添加定时事件，超时后将自动关闭该 端口*/
-        //chm_->AddNewUser(tfd);
-        tme_->TimeEventUpdate(timeout_,tfd);//注册时间事件，如果超时没有连接的话需要清楚断开该连接
-    }
-}
-
-void epollserverhandle::ReadEvent(int tfd)
-{
-    /*将fd传递给下层的协议处理函数进行读取处理，还是说在上层进行一定的校验 */
-    if(chm_->UserReadProtocl(tfd)>0)  //进行协议解析处理，考虑使用线程池进行处理，快速的进行切换
-    { //如果协议被有效的解析后，可以设置心跳时间
-        tme_->TimeEventUpdate(timeout_,tfd);//更新注册时间，否则将进行断开，同时用于心跳检测！
-    }else{ //不合法的连接及时关闭连接
-        DelEvent(tfd);
-    }
-}
-
-void epollserverhandle::DelEvent(int tfd)
-{
-    evp_->EpollDelEvent(tfd);
-    chm_->UserRemoveUser(tfd); //从队列中删除；
-    ::close(tfd);
-}
 
 int timeevent::TimeEventUpdate(size_t milliseconds,int fd)
 {
@@ -278,7 +161,21 @@ int timeevent::TimeEventUpdate(size_t milliseconds,int fd)
     tv.tv_usec += milliseconds * 1000;
     tv.tv_sec += (milliseconds / 1000) + tv.tv_usec/1000000;
     tv.tv_usec = tv.tv_usec %1000000;
-    ump[fd]= tv;
+    ump_[fd]= tv;
+    return 0;
+}
+
+int timeevent::TimeEventUpdate(size_t milliseconds,int fd,void* handle)
+{
+    /* 不管fd是否已经存在，都进行更新/添加新的时间事件*/
+    struct timeval tv;
+
+    gettimeofday(&tv, NULL);
+    
+    tv.tv_usec += milliseconds * 1000;
+    tv.tv_sec += (milliseconds / 1000) + tv.tv_usec/1000000;
+    tv.tv_usec = tv.tv_usec %1000000;
+    ump_[fd]= tv;
     return 0;
 }
 
