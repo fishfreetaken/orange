99082e1307227e0f849fc817721125c5d45a3671
chendong <471088280@qq.com>
2019-06-22 18:27:54 +0800

    server处理类

---
+++ Emperor/epollserverhandle.h
@@ -0,0 +1,37 @@
+
+
+
+#include "epollevent.h"
+#include <memory>
+class channel;
+class  epollserverhandle:public epollhandlebase{
+public:
+    epollserverhandle(int timeout=1500):
+    listen_fd_(-1),
+    timeout_(timeout)
+    {
+    }
+
+    ~epollserverhandle();
+    int ServerStart(const char* listenip,const int port);
+
+public:
+    void ReadEvent(int tfd);
+    void WriteEvent(int tfd);
+    void AcceptEvent(int tfd);
+    void DelEvent(int tfd);
+
+    int WaitTimeOut();
+
+private:
+    size_t timeout_;
+    //int tevtimeout_;
+    int listen_fd_;
+
+    std::shared_ptr<epollevent> evp_;
+
+    std::shared_ptr<channel> chm_;
+
+    std::shared_ptr<timeevent> tme_;
+
+};
