3c92b1fbc2ca962b7ae60c685ffefabc99c5dd80
chendong <471088280@qq.com>
2019-06-22 18:15:30 +0800

    为编译成功修改

--- Emperor/channel.h
+++ Emperor/channel.h
@@ -1,4 +1,11 @@
 
+
+#include "protocal.h"
+#include <vector>
+#include <unordered_map>
+#include <memory>
+#include "cryptmsg.h"
+
 #define USERSUCCESS         0
 #define USERFDREADFAIL      1 /*fd读取失败 */
 #define USERBUFDECRYPTFAIL  2 /*包解密失败 */
@@ -8,45 +15,47 @@
 #define USERPOINTNULL       6 /*入参指针为空 */
 #define USERNOTFOUNDID      7  /*查无此人 */
 #define USERFAILED          8  /*daily failed */
+#define USERNOTINITIALZE    9  /* */
  
-
 class channel;
 class user{
 public:
-    //user(size_t uid,int fd);
-    //user(size_t uid);
-    user(int fd,channel *chm);
+    user(int fd,size_t uid);
+    ~user();
 
-    int ParsePacket(transfOnPer *info);
+    int ParsePacket(int tfd);
+    size_t GetUid();
+    int InitialMyInfo(transfOnPer &m,transfPartner &s,channel *p);
 
+    int SendMyself(const transfOnPer *info);
+
+private:
     /*
         消息投递，1 对方在线；2 对方不在线，投递到一个消息队列中，检测等待上线；
     */
-    int SendTo(transfOnPer *info);
-    size_t SendToAll(transfOnPer *info);
-    int SendMyself(transfOnPer *info);
+    int SendTo();
+    size_t SendToAll();
+    
+    void HeartBeat();
 
-    /*
-        上下线管理，设置自身上下线状态，通知好友更改状态；
-    */
-    int BufferCryptCrc();
-    int BufferDecryptCrc();
-    int MysqlFileStore();
-private:
-    int SendBuf(const char* buf,size_t len);
-    int InformPartnerOnOrOff(int ret);
-    int InformPartnerOnline(user*p,size_t ret);
-    int LoadUserInfoFromDb(size_t &t); //从数据库加载
+    int InformPartnerOnline(user *handle, const size_t state);
+
+    int LoadUserInfoFromDb(size_t &uid, std::vector<transfPartner>& p);
+
+    int GenericSend(uint32_t id, const size_t &dest,const char*buf,int size,size_t from);
 private:
     size_t uid_; //使用者的uid
-    std::unoredered_map<size_t,user*> partnermap; //将所有的使用一个map进行管理起来,没上线的nullptr
+    /*内部需要维护一个朋友队列吗? */
+    std::unordered_map<size_t,std::shared_ptr<user> > partnermap_; //将所有的使用一个map进行管理起来,没上线的nullptr，不应该内部维护的一个朋友队列
 
-    char * buffer_;
+    transfOnPer recvpacket_;
+    transfOnPer sendpacket_;
 
-    //channel *chmt; //用来进行当前所有在线用户的管理；单例模式
     int fd_;
-
-    char *key_; //秘钥
+    
+    transfPartner myinfo_;
+    std::shared_ptr<cryptmsg> aescrpty_;
+    char *crpty_;    /*用户登陆密码 */
 };
 
 /*
@@ -56,27 +65,30 @@ class channel{
 public:
     channel();
 
-    static channel* GetInstance();
+    //static channel* GetInstance();
 
     /*传递给user去处理fd事件协议 */
     int UserReadProtocl(int fd);
 
-    void FindUid(int &uid);
+    std::shared_ptr<user> FindByUid(size_t&uid);
+
+    int CheckUidIsInDb(transfOnPer &m,transfPartner &s);
     /*
         从文件，或者mysql加载已经注册的有效用户
     */
     //int LoadPartnerMap(); //没有必要所有的都进行加载，进行动态的增加用户和释放
+    void UserRemove(int &tfd); //清楚用户
 
 private:
-    void UserRemove(int &tfd); //清楚用户
-    
+
+    transfOnPer sendpacket_;
+
 
 private: //能支持1千万的并发量就已经很不错了！
-    std::unordered_map<int,user*> fdmapuser_;  //该chennl下所有的partner,由客户端文件描述符进行索引 fd->id->fd进行转发消息；
+    std::unordered_map<int,std::shared_ptr<user> > fdmapuser_;  //该chennl下所有的partner,由客户端文件描述符进行索引 fd->id->fd进行转发消息；
 
-    std::unordered_map<size_t,user*> idmapuser_; //管理当前所有的user，由user id 进行索引；
+    std::unordered_map<size_t,std::shared_ptr<user> > idmapuser_; //管理当前所有的user，由user id 进行索引；
 
-/*通过参数配置的方式进行线程池的配置初始化 */
-    std::thread *threadpool_;
-
+    std::shared_ptr<cryptmsg> rsacrpty_;
 };
+
