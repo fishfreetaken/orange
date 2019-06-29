f43f97d47613c66eb51df31d7367d939c2b99368
chendong <471088280@qq.com>
2019-06-29 17:35:03 +0800

    Update log.h

--- Emperor/log.h
+++ Emperor/log.h
@@ -1,6 +1,13 @@
 #include <cstdarg>
 #include <stdio.h>
 
+/*general error! */
+#define GENERALESUCCESS 0
+#define GENERALERROR -1
+#define GENERALNOTFOUND -2
+
+
+/*log level */
 #define UTILLOGLEVELERROR       1
 #define UTILLOGLEVELRECORD      2
 #define UTILLOGLEVELWORNNING    3
