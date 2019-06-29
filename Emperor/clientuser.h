8fb82da4f75eda8ecb78fe3350437818073af276
chendong <471088280@qq.com>
2019-06-29 17:29:16 +0800

    Update clientuser.h

--- Emperor/clientuser.h
+++ Emperor/clientuser.h
@@ -1,91 +1,84 @@
 
 
-#include "protocal.h"
-#include "epollevent.h"
 #include "cryptmsg.h"
-#include <memory>
+#include "util.h"
 
-#define  CLIENTREADBUFFERLEN  512
-#define  CLIENTWRITEBUFFERLEN 512
+cryptmsg::cryptmsg(const char* s,int len)
+{
+    AcquireRsaPubKey();
+    LOG::record(UTILLOGLEVELRECORD,"client server cryptmsg create : %s\n",s);
+  //  AESGenEnCryptKey();
+}
 
-#define VECTORBUFFERLEN 50
+cryptmsg::cryptmsg()  /*server端 */
+{
+    AcquireRsaSecKey();
+    //AESGenEnCryptKey();
+}
+cryptmsg::~cryptmsg()
+{
+}
 
-#if 0
-typedef struct friendsstruct{
-    int  fd;             //文件fd
-    int  uid;
-    size_t state;
-    std::string name;   //名称
-    std::string signature;
-} friends;
-#endif
+/*新建的aes的密码体系，使用解密进行初始化 */
+/*
+void cryptmsg::cryptmsg(std::string &aesdekey);
+{
+    aesdekey_.assign(aesdekey);
+}
+*/
 
-class epollclienthandle: public epollhandlebase{
+int cryptmsg::AcquireRsaPubKey()
+{
+    rsapubkey_.assign("123456778");
+}
 
-public:
-    epollclienthandle(size_t uid);
-    ~epollclienthandle();
-    int StartConnect(const char* listenip,int port);
+int cryptmsg::AcquireRsaSecKey()
+{
+    rsaseckey_.assign("123456778");
+}
 
-public:
-    void ReadEvent(int tfd);
-    void WriteEvent(int tfd); //标准输出端口输出
-    void AcceptEvent(int tfd);
-    void DelEvent(int tfd);
-    int WaitTimeOut();
+int cryptmsg::AESDecrypt(const char* in,int size, char * packet)
+{
+    return 0;
+}
 
-private:
-    int FormMsgAddToBuffer(uint32_t msgid,const char*buf,int len);
-    int MsgSendFromStd0();
+int cryptmsg::AESEncrypt(const char* packet,int size,char *out )
+{
+    return 0;
+}
 
+int cryptmsg::RSADecrypt(const char* in,int size, char * packet)
+{
+    AcquireRsaSecKey();
+    return 0;
+}
 
-    int PushMsgToTerminal(transfOnPer *p);
-    void AddNewFriends(transfOnPer *p);
-    void InintialMyInfo(transfOnPer *p);
+int cryptmsg::RSAEncrypt(const char* packet,int size,char *out )
+{
+    return 0;
+}
 
-    void UserSendMsgPoll();
+/* 
+int cryptmsg::AESSetDecryptKey(std::string &m)
+{
+    aesdekey_.assign(m);
+}
+*/
 
-    void CalculateCrc(transfOnPer *p);
+int cryptmsg::AESCallEnCryptKey(std::string &m)
+{
+    //m.copy(aesenkey_);
+}
 
-    transfOnPer * SendBufferPush();
-    transfOnPer * SendBufferPop();
+int cryptmsg::AESGenEnCryptKey(char *s,int len)
+{
+    aesenkey_.assign("16gr4eg");
+    std::string m="AESGenEnCryptKey";
+    if(len<m.size())
+    {
+        LOG::record(UTILLOGLEVELWORNNING, "AESGenEnCryptKey len : %d < local:%d", len,m.size());
+    }
+    m.copy(s,len);
+}
 
-private :
-    int fd_;
-    size_t uid_;
-    std::shared_ptr<epollevent> evp_;
-
-    std::shared_ptr<cryptmsg> crypt_;
-
-    //std::vector<transfOnPer> recvbuffer_;
-    std::vector<transfOnPer> sendbuffer_;
-
-    char *readbuffer_;
-    char *serverbuffer_;
-
-    //int recv_pos_begin_;
-    //int recv_pos_end_;
-
-    int send_pos_begin_;
-    int send_pos_end_;
-
-    std::shared_ptr<timeevent> tme_;
-    std::vector<int> timerollback_;
-    transfPartner myinfo_;
-
-    std::map<size_t, transfPartner> myfriends_;
-    size_t curdialog_; /*当前对话的伙伴 */
-
-    size_t recpackagecount_;
-    size_t sendpackagecount_;
-};
-
-
-#if 0
-//使用Std的删除函数
-std::shared_ptr<int> sp(new int[10], std::default_delete<int[]>());
-
-//使用lambda表达式
-std::shared_ptr<int> sp(new int[10], [](int *p) { delete[] p; });
-#endif
 
