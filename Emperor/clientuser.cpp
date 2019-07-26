69ac4a37d152a6f78508fa8d09d97caf1041bcd1
chendong <471088280@qq.com>
2019-07-26 17:24:26 +0800

    使用新protocal进行重构

--- Emperor/clientuser.cpp
+++ Emperor/clientuser.cpp
@@ -3,36 +3,19 @@
 #include "util.h"
 #include <signal.h>
 
-
-
-epollclienthandle::epollclienthandle(size_t uid):
-fd_(0),
-uid_(uid),
+/* */
+epollclienthandle::epollclienthandle(uint32_t uid,char *password):
+fd_(-1),
 curdialog_(0),
-send_pos_begin_(-1),
-send_pos_end_(-1),
-recpackagecount_(0),
-sendpackagecount_(0),
-heart_count_(0)
+myinfo_.uiId(uid),
+myinfo_.sPassword.assign(password),
+epolltimeout_(-1),
+uiHeartInterval_(1000) //1s的间隔
 {
-    try{
-        readbuffer_= new char[CLIENTREADBUFFERLEN];
-        serverbuffer_ = new char[CLIENTWRITEBUFFERLEN];
-    }catch(...)
-    {
-        throw strerror(errno);
-    }
-    
-    sendbuffer_.resize(VECTORBUFFERLEN);
-    timerollback_.resize(VECTORBUFFERLEN);
-    //recvbuffer_.resize(VECTORBUFFERLEN);
 }
 
 epollclienthandle::~epollclienthandle()
 {
-    delete [] readbuffer_;
-    delete [] serverbuffer_;
-
     if(fd_>0)
     {
         ::close(fd_);
@@ -41,8 +24,9 @@ epollclienthandle::~epollclienthandle()
 
 int epollclienthandle::WaitTimeOut()
 {
-    return 1000;
+    return epolltimeout_;
 }
+
 
 void SingalCallBack(int signale_number)
 {
@@ -51,106 +35,61 @@ void SingalCallBack(int signale_number)
 
 int epollclienthandle::StartConnect(const char* listenip,int port)
 {
-    if(uid_==0)
-    {
-        LOG::record(UTILLOGLEVELERROR, "not valid uid");
-        return UTILNET_ERROR; 
-    }
 
     fd_=tcpGenericConnect(NULL,0,listenip,port);
-    if(fd_<0)
-    {
-        LOG::record(UTILLOGLEVELERROR, "tcpGenericConnect %d: %s",errno,strerror(errno));
-        return UTILNET_ERROR;
-    }
-
-    setNonBlock(fd_);
+    if(fd_<0) {return Status::mIOError;}
+    recpacket_.SetFd(fd_);
 
     try
     {
         evp_=std::make_shared<epollevent>(this,fd_);
         tme_=std::make_shared<timeevent>();
-        crypt_=std::make_shared<cryptmsg>("./rsapub.pem");
+        aescrypt_=std::make_shared<Aescrypt>();
+        sendqueue_=std::make_shared<SerialBuffer>();
+        recpacket_ = std::make_shared<Protocal>(fd_);
     }catch (...){
-        throw  strerror(errno);
+        throw  "clientuser new failed";
     }
 
     /*加密协议消息 */
-    transfOnPer st;
-    std::memset(&st,0,STRUCTONPERLEN);
-    transfcrptykey *key=(transfcrptykey*)st.buf;
-    std::memset(key,0,sizeof(transfcrptykey));
+    /*直接使用用户名密码进行替换，生成256字节的对称加密秘钥；sha(sha(随机串))+sha(id)+sha(密码)+sha(id+密码)+shasha(id)+shasha(密码)+shasha(id+密码)+sha(随即串) */
 
-    crypt_->AESGenEnCryptKey((unsigned char*)key->key,0);
-
-    snprintf(key->secret,LOADPERSONCRTPYLEN,"dadonggeSecret");/*这个地方后边要用md5 */
-    crypt_->Sha256Hash((unsigned char*)key->secret,strlen(key->secret),(unsigned char*)key->secret,LOADPERSONCRTPYLEN);
-
-    st.id=MSGSECRET;
-    st.uid=uid_;
-
-    genCrcPayload(st);
-
-    int ret=crypt_->RSAEncrypt((unsigned char*)&st,STRUCTONPERLEN,(unsigned char*)serverbuffer_,CLIENTWRITEBUFFERLEN);
-    if(ret<RSAENCRYPTBUFLEN)
+    /*生成完直接拷贝到这个结构体中，设置myinf_的字符串*/
+    if(aescrypt_->AesKeyGen(myinfo_)!=Statue::mOk)
     {
-        GENERRORPRINT("RSAEncrypt failed!",ret,RSAENCRYPTBUFLEN);
-        return UTILNET_ERROR;
+        LogError("AesKeyGen generator failed %d",ret);
     }
 
-    ret=writeGenericSend(fd_,serverbuffer_,ret);
-    if(ret<=0)
-    {
-        GENERRORPRINT("writeGenericSend",ret,0);
-        return UTILNET_ERROR;
-    }
+    sendqueue_.push(Protoal(fd_,Protocaltype::mSecret,myinfo_));
 
-    tme_->TimeEventUpdate(1000,fd_);
+    tme_->TimeEventAdd(fd_,uiHeartInterval_);/*server 心跳事件 */
 
     /*标准输入输出 */
-    setNonBlock(0);
+    //setNonBlock(0);
     struct epoll_event ee = {0,0};
     ee.events |=  EPOLLIN | EPOLLET;
     ee.data.fd=0; //注册用户标准输入stdio 0事件；
-
     evp_->EpollEventAdd(ee);
 
     //signal(SIGPIPE,SingalCallBack);
     /*当主控断开后，会由于SIGPIPE信号导致进程中断，这个也是断线重连机制的基础 */
     signal(SIGPIPE,SIG_IGN);
-    std::memset(&myinfo_,0,STRUCTONFRILEN);
 
-    ret=0;
     while(1)
     {
-        ret=evp_->EpollEventWaite();//程序主入口，设置的轮训时间是1s，跳出来之后进行发送心跳包
-        if(ret<0)
+        //程序主入口，按照redis的方法，对于下次的轮训时间设置为最近快要过期的时间
+        if(evp_->EpollEventWaite()<0)
         {
-            LOG::record(UTILLOGLEVELWORNNING, "EpollEventWaite LINE=%d error ret=%d\n",__LINE__,ret);
-            break;
+            LogError("EpollEventWaite error %s\n",strerror(errno));
         }
-        if(myinfo_.state==0)
-        {
-            //printf("myinfo_.state==0\n");
-            continue;
-        }
+        /*执行定时事件 */
+        epolltimeout_=tme_->TimeEventProc(this);
 
-        ret=tme_->TimeEventProc(timerollback_);
-        if(ret>0)
-        {
-            tme_->TimeEventUpdate(1500,fd_);
-            //向server发送
-            heart_count_++;
-            FormMsgAddToBuffer(MSGHEART,(char*)&heart_count_,sizeof(size_t),0);//1s发送一个心跳包
-        }
-
-        //先将个人信息写入发送至服务端获取个人信息；
-        if(UserSendMsgPoll()!=UTILNET_SUCCESS)
-        {/*发送数据失败，断开连接 */
-            return UTILNET_ERROR;
-        }
+        /*发送消息 */
+        sendqueue_.pop();
     }
-    return UTILNET_SUCCESS;
+
+    return Status::mOk;
 }
 
 /*从server端收到的数据 */
@@ -158,94 +97,44 @@ void epollclienthandle::AcceptEvent(int fd)
 {
     int ret=0;
 
-while(1)/*一次并不能读完 */
-{
-    std::memset(serverbuffer_,0,CLIENTREADBUFFERLEN);
-    std::memset(&recvpacketbuf_,0,STRUCTONPERLEN);
-    ret=readGenericReceive(fd_,serverbuffer_,STRUCTONPERLEN);
-    /*ret是int类型带符号，STRUCTONPERLEN是无符号类型，当ret<0时候，ret会大于STRUCTONPERLEN，这样比较会有问题 */
-    if((ret< STRUCTONPERLEN )||(ret<0))
+    while(recpacket_.ReceivePkg(aescrypt_)==Status::mOk)/*一次并不能读完 */
     {
-        /*ret=0表示对端已经断开 */
-        /*这里应该将buf内容进行二进制的输出到log上 */
-        if((errno == EINTR)||(errno == EAGAIN))
+        switch(p->id)
         {
-            break;
+            case MSGHEART:
+                //printf("Server reback num %zu\n",*(p->buf));
+                break;
+            case MSGFRIEND:
+                PushMsgToTerminal(p);
+                break;
+            case MSGSERVERINFO:
+                InintialMyInfo(p);
+                break;
+            case MSGFRIENDINFO:
+                AddNewFriends(p);
+                break;
+            default:
+                LOG::record(UTILLOGLEVELWORNNING, "not recoginsed tid: %d", p->id);
+                return;
+                break;
         }
-        LOG::record(UTILLOGLEVELWORNNING, "readGenericReceive %s LINE:%d  ret:%d errno=%d", strerror(errno),__LINE__,ret,errno);
-        break;
-    }
-
-    transfOnPer *p=&recvpacketbuf_;
-    #ifdef NEEDAESCRYPT
-        ret=crypt_->AESDecrypt((unsigned char*)serverbuffer_,ret,(unsigned char*)p,STRUCTONPERLEN);
-        if(ret!=0)
-        {
-            GENERRORPRINT("AESDecrypt failed",ret,0);
-            return ;
-        }
-    #else
-        std::memcpy(&recvpacketbuf_,serverbuffer_,STRUCTONPERLEN);
-    #endif
-
-    /*crc校验，这样校验不过应该需要从server端断开连接的 */
-    if(verifyCrcPayload(*p)!=UTILNET_SUCCESS)
-    {
-        LOG::record(UTILLOGLEVELRECORD,"epollclienthandle::AcceptEvent crc query failed uid:%zu",uid_);
-        printTransfOnPer(p, "AcceptEvent crc query failed");
-        return ;
-    }
-
-    if(p->to!=uid_)
-    {
-        printTransfOnPer(p, "AcceptEvent UID mismatch");
-        LOG::record(UTILLOGLEVELERROR, "zero dest uid, invalid! to:%zu",p->to);
-        return ;
-    }
-
-    recpackagecount_++;
-
-    switch(p->id)
-    {
-        case MSGHEART:
-            //printf("Server reback num %zu\n",*(p->buf));
-            break;
-        case MSGFRIEND:
-            PushMsgToTerminal(p);
-            break;
-        case MSGSERVERINFO:
-            InintialMyInfo(p);
-            break;
-        case MSGFRIENDINFO:
-            AddNewFriends(p);
-            break;
-        default:
-            LOG::record(UTILLOGLEVELWORNNING, "not recoginsed tid: %d", p->id);
-            return;
-            break;
     }
 }
-ShowPacketInfo();
-}
 
-void epollclienthandle::ShowPacketInfo()
+void epollclienthandle::TimeoutEvent(int tfd, timeevent *t);
 {
-    static size_t locount=0;
-    if((recpackagecount_!=(sendpackagecount_+2)))
+    /*服务器定时心跳事件 */
+    if(tfd==fd_)
     {
-        printf("uid:%zu recpackagecount_ :%zu  sendpackagecount_ :%zu locount=%zu\n",uid_,recpackagecount_,sendpackagecount_,locount);
+        sendqueue_.push(Protoal(fd_,Protocaltype::mHeart,myinfo_));
+        tme_->TimeEventAdd(fd_,uiHeartInterval_);/*server 心跳事件 */
     }
-    printf("uid:%zu recpackagecount_ :%zu  sendpackagecount_ :%zu locount=%zu\n",uid_,recpackagecount_,sendpackagecount_,locount);
-
-    locount=recpackagecount_;
-    
 }
 
 void epollclienthandle::DelEvent(int tfd)
 {
     evp_->EpollDelEvent(tfd);
     ::close(tfd);
-    LOG::record(UTILLOGLEVELRECORD,"DelEvent close fd: %d\n",tfd);
 }
 
 void epollclienthandle::WriteEvent(int tfd)
@@ -254,23 +143,28 @@ void epollclienthandle::WriteEvent(int tfd)
     {
         UserSendMsgPoll();
     }
-    LOG::record(UTILLOGLEVELERROR, "reservered %d", tfd);
+    //LOG::record(UTILLOGLEVELERROR, "reservered %d", tfd);
     return ;
 }
 
 /*标准设备读入事件 */
 void epollclienthandle::ReadEvent(int tfd)
 {
-    switch(tfd)
+    int ret;
+    std::memset(readbuffer_,0,CLIENTWRITEBUFFERLEN);
+    ret=readGenericReceive(0,readbuffer_,CLIENTREADBUFFERLEN);
+    if(ret<0)
     {
-        case 0: //从标准输入设备中读入数据；
-            MsgSendFromStd0();
-            break;
-        default:
-            LOG::record(UTILLOGLEVELERROR, "not recognisied fd %d", tfd);
-            return;
-            break;
+        return ret;
     }
+    auto it=myfriends_.begin();
+    if(it==myfriends_.end())
+    {
+        GENERRORPRINT("I have no friends",0,0);
+        return UTILNET_ERROR;
+    }
+    
+    curdialog_=it->second.uid;
 }
 
 int epollclienthandle::UserSendMsgPoll()
@@ -281,9 +175,9 @@ int epollclienthandle::UserSendMsgPoll()
     /*表示对端主动断开连接 */
     if(errno==EPIPE)
     {
-        GENERRORPRINT("errno EPIPE",errno,0);
         errno=0;
         DelEvent(fd_);
+        LogRecord("EPIPE error fd: %d, %s\n",fd_,sterrno(errno));
         return errno;
     }
     /*网线断了，或者buf满了 */
@@ -303,7 +197,7 @@ int epollclienthandle::UserSendMsgPoll()
     return UTILNET_SUCCESS;
 }
 
-void epollclienthandle::InintialMyInfo(transfOnPer *p)
+void epollclienthandle::InintialMyInfo()
 {
     transfPartner *cc= (transfPartner *)p->buf;
     //printfPartner(cc,"InintialMyInfo");
@@ -333,39 +227,18 @@ void epollclienthandle::AddNewFriends(transfOnPer *p)
     return;
 }
 
-int epollclienthandle::PushMsgToTerminal(transfOnPer *p)
+void epollclienthandle::PushMsgToTerminal(transfOnPer *p)
 {
 
     if(myfriends_.find(p->uid)==myfriends_.end())
     {
         LOG::record(UTILLOGLEVELERROR, "not my friend %zu",p->uid);
-        return PROTOCALNOTFOUND;
     }
-    printTransfOnPer(p,"PushMsgToTerminal");
+    
 
     return PROTOCALSUCCESS;
 }
 
-/*从终端输入的信息发送至服务端 */
-int epollclienthandle::MsgSendFromStd0()
-{
-    int ret;
-    std::memset(readbuffer_,0,CLIENTWRITEBUFFERLEN);
-    ret=readGenericReceive(0,readbuffer_,CLIENTREADBUFFERLEN);
-    if(ret<0)
-    {
-        return ret;
-    }
-    auto it=myfriends_.begin();
-    if(it==myfriends_.end())
-    {
-        GENERRORPRINT("I have no friends",0,0);
-        return UTILNET_ERROR;
-    }
-    
-    curdialog_=it->second.uid;
-    FormMsgAddToBuffer(MSGFRIEND,readbuffer_,ret,curdialog_);
-}
 
 int epollclienthandle::FormMsgAddToBuffer(uint32_t msgid,const char*buf,int len,size_t to)
 {
@@ -398,85 +271,3 @@ int epollclienthandle::FormMsgAddToBuffer(uint32_t msgid,const char*buf,int len,
 
     return UTILNET_SUCCESS;
 }
-
-transfOnPer * epollclienthandle::SendBufferPush()
-{
-    int len=sendbuffer_.size();
-    send_pos_begin_++;
-    if(send_pos_begin_==len)
-    {
-        send_pos_begin_=0;
-    }
-    if(send_pos_begin_==send_pos_end_) //buffer满了
-    {
-        return nullptr;
-    }
-
-    transfOnPer * p= &sendbuffer_[send_pos_begin_];
-
-    return p;
-}
-
-#if 0
-transfOnPer * epollclienthandle::SendBufferPop()
-{
-    int len=sendbuffer_.size();
-
-    if(send_pos_begin_==send_pos_end_) //buffer空的
-    {
-        return nullptr;
-    }
-    send_pos_end_++;
-    if(send_pos_end_==len)
-    {
-        send_pos_end_=0;
-    }
-
-    transfOnPer * p= &sendbuffer_[send_pos_end_];
-
-    return p;
-}
-#else
-int epollclienthandle::SendBufferPop()
-{
-    int len=sendbuffer_.size();
-    int ret=0;
- 
-    if(send_pos_begin_==send_pos_end_) //buffer空的
-    {
-        return -1;
-    }
-
-    transfOnPer p;
-
-    int tmp=send_pos_end_;
-    tmp++;
-    if(tmp==len)
-    {
-        tmp=0;
-    }
-
-    std::memcpy(&p,&sendbuffer_[tmp],STRUCTONPERLEN); 
-
-    /*到发送的时候再进行加密 */
-    #ifdef NEEDAESCRYPT
-        ret=crypt_->AESEncrypt((unsigned char*)&p,STRUCTONPERLEN,(unsigned char*)&p,STRUCTONPERLEN);
-        if(ret!=0)
-        {
-            GENERRORPRINT("RSAEncrypt error",ret,0);
-            return UTILNET_ERROR;
-        }
-    #endif
-
-    ret=writeGenericSend(fd_,(char*)&p, STRUCTONPERLEN);
-    if(ret<0)
-    {
-        return ret;
-    }
-
-    /*发送成功了才进一步 */
-    send_pos_end_=tmp;
-
-    return UTILNET_SUCCESS;
-}
-#endif
